c7e8688e6085b4cd70fcdbfa962bf9a9d3b29d7f
DJ Schleen <djschleen@gmail.com>
2021-04-05 13:11:45 -0600

    Updated remove description to add more detail

--- cmd/remove.go
+++ cmd/remove.go
@@ -10,8 +10,8 @@ var (
 	removeCmd = &cobra.Command{
 		Use:     "remove",
 		Aliases: []string{"delete"},
-		Short:   "Removes the hooks as defined in the .hooks.yaml file.",
-		Long:    "Removes the hooks as defined in the .hooks.yaml file.",
+		Short:   "Removes the hooks as defined in the .hooks.yaml file and any generated scripts.",
+		Long:    "Removes the hooks as defined in the .hooks.yaml file and any generated scripts.",
 		Run: func(cmd *cobra.Command, args []string) {
 			hookzHeader()
 			fmt.Println("[*] Removing existing hooks...")
