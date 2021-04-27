8868a9654dd2cdd785ce410ccbb77b5715cfbdfc
DJ Schleen <djschleen@gmail.com>
2021-04-27 10:07:38 -0600

    Extracted all logic to lib

--- lib/error.go
+++ lib/error.go
@@ -1,17 +1,22 @@
+//Package lib Functionality for the Hookz CLI
 package lib
 
 import "log"
 
-func IsError(err error, pre string) error {
+//IsError Checks to see if an error exists, and if so
+//writes it to the log with the provided prefix
+func IsError(err error, prefix string) error {
 	if err != nil {
-		log.Printf("%v: %v", pre, err)
+		log.Printf("%v: %v", prefix, err)
 	}
 	return err
 }
 
-func IsErrorBool(err error, pre string) (b bool) {
+//IsErrorBool Checks to see if an error exists, and if so
+//returns true after writing the error to the log with the provided prefix
+func IsErrorBool(err error, prefix string) (b bool) {
 	if err != nil {
-		log.Printf("%v: %v", pre, err)
+		log.Printf("%v: %v", prefix, err)
 		b = true
 	}
 	return
