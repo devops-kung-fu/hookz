32b784d796fcaa2d7c063608aa9025e6e187f839
DJ Schleen <djschleen@gmail.com>
2021-07-01 23:26:58 -0600

    Removes dead DownloadURL code

--- lib/web.go
+++ lib/web.go
@@ -99,52 +99,6 @@ func DownloadFile(fs FileSystem, filepath string, URL string) (filename string, 
 	return
 }
 
-// func DownloadURL(URL string) (filename string, err error) {
-// 	URL = platformURLIfDefined(URL)
-// 	_, err = url.ParseRequestURI(URL)
-// 	if err != nil {
-// 		return
-// 	}
-// 	client := grab.NewClient()
-// 	req, err := grab.NewRequest(".git/hooks", URL)
-// 	if err != nil {
-// 		return
-// 	}
-
-// 	fmt.Printf("Downloading %v...\n", req.URL())
-// 	resp := client.Do(req)
-// 	fmt.Printf("  %v\n", resp.HTTPResponse.Status)
-
-// 	t := time.NewTicker(500 * time.Millisecond)
-// 	defer t.Stop()
-
-// Loop:
-// 	for {
-// 		select {
-// 		case <-t.C:
-// 			fmt.Printf("  transferred %v / %v bytes (%.2f%%)\n",
-// 				resp.BytesComplete(),
-// 				resp.Size,
-// 				100*resp.Progress())
-
-// 		case <-resp.Done:
-// 			break Loop
-// 		}
-// 	}
-
-// 	if err := resp.Err(); err != nil {
-// 		fmt.Fprintf(os.Stderr, "Download failed: %v\n", err)
-// 		return resp.Filename, err
-// 	}
-
-// 	fmt.Printf("Download saved to ./%v \n", resp.Filename)
-// 	err = os.Chmod(resp.Filename, 0777)
-// 	if err != nil {
-// 		return resp.Filename, err
-// 	}
-// 	return resp.Filename, err
-// }
-
 func platformURLIfDefined(URL string) string {
 	return strings.Replace(URL, "%%PLATFORM%%", getPlatformName(), 1)
 }
